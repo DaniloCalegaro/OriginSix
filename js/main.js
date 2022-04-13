fed1929433f292728a2f893d2ad27bfe1d000f00
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-04-13 13:19:22 -0300

    Finalizando responsividade

--- js/main.js
+++ js/main.js
@@ -22,7 +22,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 //--------ScrollReveal----------
