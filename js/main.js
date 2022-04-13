788ef35f6db7c634c349cc50e1edf2c563699bc1
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-04-13 17:46:21 -0300

    Ativando itens do menu por deslocamento de pagina

--- js/main.js
+++ js/main.js
@@ -50,10 +50,10 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('.header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('.header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -70,8 +70,35 @@ function backToTop() {
   }
 }
 
+//-------Active menu selected--------
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 //------- When Scroll ---------
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
